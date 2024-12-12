1873ee4712c2f98cda5f03cc9b2722028501be4e
Andre Freitas <freitasandre38@gmail.com>
2024-12-12 15:26:55 -0300

    fix: solve problems with the dockerfile image, change deployment envs, adjust the name from configmp key ref

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM golang:1.15
+FROM golang:1.16
+WORKDIR /app
+
 COPY . .
+
+RUN go mod init server || true
+RUN go mod tidy 
 RUN go build -o server .
+
 CMD ["./server"]
--- server.go
+++ server.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/envs", ConfigMap)
+	http.HandleFunc("/secret", Secret)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -16,4 +19,20 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	user := os.Getenv("USER-CONTEXT")
 	date := os.Getenv("DATE")
 	fmt.Fprintf(w, "User-Context: %s | %s", user, date)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("go/envs/envs.txt")
+	if err != nil {
+		log.Fatalf("Error until read file", err)
+	}
+
+	fmt.Fprintf(w, "Envs: %s", string(data))
+
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s \nPassword: %s", user, password)
 }
